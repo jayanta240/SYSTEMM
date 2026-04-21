da0a9f28041e64198c884eba8f68b8cad68f1c3d
Jayanta Chakraborty <jayanta420.ch@gmail.com>
2026-04-22 03:01:44 +0530

    Use regex CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -12,15 +12,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://monorepo-ebon-eight.vercel.app"
-    ],
+    allow_origins=["http://localhost:3000"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 os.makedirs("temp", exist_ok=True)
 
 processed_files = set()
