26e02371b1acfb23c6eb99ae14d58d5bb8d1b9e8
Jayanta Chakraborty <jayanta420.ch@gmail.com>
2026-04-22 02:52:35 +0530

    Fix CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://monorepo-ebon-eight.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
